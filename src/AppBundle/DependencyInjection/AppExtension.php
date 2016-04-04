6b92b369e2613134f78aa26ea7554deab4300db6
Grégoire Pineau <lyrixx@lyrixx.info>
2016-04-04 22:17:32 +0200

    Move some code to symfony

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -16,49 +16,5 @@ class AppExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-        $loader->load('symfony.xml');
-
-        $configuration = $this->getConfiguration($configs, $container);
-
-        $config = $this->processConfiguration($configuration, $configs);
-
-        if (isset($config['workflow']) && $config['workflow']) {
-            $this->buildWorkflow($container, $config['workflow']);
-        }
-    }
-
-    private function buildWorkflow(ContainerBuilder $container, array $workflows)
-    {
-        $registryDefintion = $container->getDefinition('workflow.registry');
-
-        foreach ($workflows as $name => $workflow) {
-            $definitionDefinition = new Definition('Symfony\Component\Workflow\Definition');
-            $definitionDefinition->addMethodCall('addPlaces', [$workflow['places']]);
-            foreach ($workflow['transitions'] as $transitionName => $transition) {
-                $definitionDefinition->addMethodCall('addTransition', [new Definition('Symfony\Component\Workflow\Transition', [$transitionName, $transition['from'], $transition['to']])]);
-            }
-
-            if (isset($workflow['marking_store']['type'])) {
-                $markingStoreDefinition = new DefinitionDecorator('workflow.marking_store.'.$workflow['marking_store']['type']);
-                foreach ($workflow['marking_store']['arguments'] as $arg) {
-                    $markingStoreDefinition->addArgument($arg);
-                }
-            } else {
-                $markingStoreDefinition = new Reference($workflow['marking_store']['service']);
-            }
-
-            $workflowDefinition = new DefinitionDecorator('workflow.abstract');
-            $workflowDefinition->replaceArgument(0, $definitionDefinition);
-            $workflowDefinition->replaceArgument(1, $markingStoreDefinition);
-            $workflowDefinition->replaceArgument(3, $name);
-
-            $workflowId = 'workflow.'.$name;
-
-            $container->setDefinition($workflowId, $workflowDefinition);
-
-            foreach ($workflow['supports'] as $supportedClass) {
-                $registryDefintion->addMethodCall('add', [new Reference($workflowId), $supportedClass]);
-            }
-        }
     }
 }
