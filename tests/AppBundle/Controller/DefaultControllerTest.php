5d523912898cbd4401f7ed20f8bac2d9da8c6f31
Grégoire Pineau <lyrixx@lyrixx.info>
2016-11-09 17:22:31 +0100

    Fixed tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -13,6 +13,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('symfony/workflow', $crawler->filter('.container h1')->text());
     }
 }
