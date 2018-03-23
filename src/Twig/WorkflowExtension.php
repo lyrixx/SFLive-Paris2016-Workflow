690307886d78d2812041656fb3c9c2ba0f366165
Grégoire Pineau <lyrixx@lyrixx.info>
2018-03-23 15:38:27 +0100

    Added some example about transition blockers

--- src/Twig/WorkflowExtension.php
+++ src/Twig/WorkflowExtension.php
@@ -17,16 +17,24 @@ class WorkflowExtension extends \Twig_Extension
     {
         return array(
             new \Twig_SimpleFunction('workflow_all_transitions', [$this, 'getTransitions']),
+            new \Twig_SimpleFunction('workflow_build_transition_blocker_list', [$this, 'buildTransitionBlockerList']),
         );
     }
 
-    // This method is a hack to get all transitions, enable-able or not.
+    // This method is a hack to get all transitions, enabled or not.
     // This should be done only for a demo purpose
-    public function getTransitions($object, $name = null)
+    public function getTransitions($subject, string $name = null)
     {
-        $workflow = $this->workflowRegistry->get($object, $name);
+        $workflow = $this->workflowRegistry->get($subject, $name);
 
         return $workflow->getDefinition()->getTransitions();
+    }
+
+    public function buildTransitionBlockerList($subject, string $transitionName, string $name = null)
+    {
+        $workflow = $this->workflowRegistry->get($subject, $name);
+
+        return $workflow->buildTransitionBlockerList($subject, $transitionName);
     }
 
     public function getName()
