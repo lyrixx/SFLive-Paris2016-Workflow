c2f43f7a1948adbc389b4d3275d2853a5bd9312d
Grégoire Pineau <lyrixx@lyrixx.info>
2019-11-25 16:45:56 +0100

    Upgrade to SF 5.0

--- config/bundles.php
+++ config/bundles.php
@@ -8,7 +8,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
