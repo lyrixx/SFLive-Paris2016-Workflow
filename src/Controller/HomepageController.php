ae4fd3faab364bcbb40abf545de7646a364da9cd
Grégoire Pineau <lyrixx@lyrixx.info>
2021-02-04 22:12:20 +0100

    use new symfony security system

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -14,4 +14,20 @@ class HomepageController extends AbstractController
     {
         return $this->render('homepage/index.html.twig');
     }
+
+    /**
+     * @Route("/login/{username}", name="login")
+     */
+    public function login()
+    {
+        throw new \LogicException('The security component should handle this route');
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('The security component should handle this route');
+    }
 }
