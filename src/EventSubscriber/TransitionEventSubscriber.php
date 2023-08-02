900ff20075198c302a440dd50f882ecbb129aaba
Grégoire Pineau <lyrixx@lyrixx.info>
2023-08-02 14:29:40 +0200

    Add suppor for clever cloud

--- src/EventSubscriber/TransitionEventSubscriber.php
+++ src/EventSubscriber/TransitionEventSubscriber.php
@@ -2,18 +2,21 @@
 
 namespace App\EventSubscriber;
 
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Workflow\Attribute\AsTransitionListener;
 use Symfony\Component\Workflow\Event\TransitionEvent;
 
-class TransitionEventSubscriber implements EventSubscriberInterface
+class TransitionEventSubscriber
 {
     public function __construct(
         private readonly TokenStorageInterface $tokenStorage,
     ) {
     }
 
+    #[AsTransitionListener()]
+    #[AsTransitionListener(workflow: 'my-workflow')]
+    #[AsTransitionListener(workflow: 'my-workflow', transition: 'some-transition')]
     public function onWorkflowArticleTransition(TransitionEvent $event): void
     {
         $context = $event->getContext();
--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,18 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+class Kernel extends BaseKernel implements CompilerPassInterface
 {
     use MicroKernelTrait;
+
+    public function process(ContainerBuilder $container)
+    {
+        foreach ($container->findTaggedServiceIds('workflow.workflow') as $id => $attributes) {
+            $config = $container->getDefinition($id)->getTag('workflow')[0];
+        }
+    }
 }
